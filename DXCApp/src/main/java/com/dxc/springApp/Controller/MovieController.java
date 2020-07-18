d3d0ea75f9b7edc1d59de08a0850b08ecb85f6d1
Lenovo <Lenovo@DESKTOP-HJKTJJ6>
2020-07-18 12:08:53 +0530

    dxc application assignment

--- DXCApp/src/main/java/com/dxc/springApp/Controller/MovieController.java
+++ DXCApp/src/main/java/com/dxc/springApp/Controller/MovieController.java
@@ -21,37 +21,37 @@ public class MovieController {
 	@Autowired
 	MovieService movieService;
 
-	// http://localhost:8562/employee/all
+	// http://localhost:8562/movie/all
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public List<Movie> getAllMovies() {
 		return this.movieService.getAllMovies();
 	}
 
-	// http://localhost:8562/employee/addEmployee
+	// http://localhost:8562/movie/addMovie
 	@RequestMapping(value = "/addMovie", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Movie addMovie(@RequestBody Movie movie) {
 		return this.movieService.addMovie(movie);
 	}
 
-	// http://localhost:8562/employee/updateEmployee
+	// http://localhost:8562/Movie/updateMovie
 	@RequestMapping(value = "/updateMovie", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Movie updateMovie(@RequestBody Movie movie) {
 		return this.movieService.updateMovie(movie);
 	}
 
-	// http://localhost:8562/employee/1
+	// http://localhost:8562/movie/1
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Optional<Movie> getMovie(@PathVariable int id) {
 		return this.movieService.getMovieById(id);
 	}
 
-	// http://localhost:8562/employee/all
+	// http://localhost:8562/movie/all
 	@RequestMapping(value = "/all", method = RequestMethod.DELETE)
 	public void deleteAllMovies() {
 		this.movieService.deleteAllMovies();
 	}
 
-	// http://localhost:8562/employee/1
+	// http://localhost:8562/movie/1
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void deleteMovie(@PathVariable int id) {
 		this.movieService.deleteMovieById(id);
